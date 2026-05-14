be86d76e111ab1600818bcbde8b3ffcde5490849
konradkelly <konradky@gmail.com>
2026-05-14 11:21:45 -0700

    fix: remove duplicate BrowserRouter config

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Videos from './pages/Videos.jsx'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Videos from './pages/Videos.jsx';
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <Home />
-      <Videos />
-    </>
-  )
+    <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/videos" element={<Videos />} />
+    </Routes>
+    </div>
+  );
 }
-
-export default App

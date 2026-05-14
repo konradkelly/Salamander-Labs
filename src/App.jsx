868aeec31b77928460d641a47625e248b3b4383b
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-05-14 11:21:41 -0700

    added link import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Videos from './pages/Videos.jsx';
 

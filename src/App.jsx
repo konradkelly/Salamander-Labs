937738eb8eb443ec0f4037940c029a175e0e7a80
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-05-14 11:20:27 -0700

    added nav links

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Videos from './pages/Videos.jsx';
 export default function App() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        {' | '}
+        <Link to="/videos">Videos</Link>
+      </nav>
+  
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/videos" element={<Videos />} />
